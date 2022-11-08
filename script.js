b827371ba1bfe6bd04ca928b261e65a21349fb26
Jair <jairaceves56@gmail.com>
2022-11-08 12:29:50 -0600

    Blurr-Loading

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const panels = document.querySelectorAll('.panel')
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
-panels.forEach( panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+let load = 0
+let int = setInterval(blurring, 30)
 
-function removeActiveClasses() {
-    panels.forEach( panel => {
-        panel.classList.remove('active')
-    })
+function blurring() {
+    load++
+
+    if(load > 99) {
+        clearInterval(int)
+    }
+    
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`
 }
 
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
